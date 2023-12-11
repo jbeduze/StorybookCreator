74ee36dc1159213f52060aac0a03309f3f2f39f5
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-10 22:08:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.write('Hello fools!')
 
-def Story_form():
-  with st.expander('',expander=True):
+st.form("Story_form"):
+    with st.expander('',expander=True):
 
     options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
     radio_cols = st.columns([.25,10])
@@ -12,9 +12,12 @@ def Story_form():
       st.markdown("Let's get started, please fill out the following fields:")
       lmnt_cols = st.columns(2)
       lmnt_cols[0].text_input('relation')
-      '---'
+      lmnt_cols[1].text_input('Name')
+
+      submitted = st.form_submit_button('Pull Elements')
 
 
-def Elements_pool():
-  st.file_uploader(), st.file_uploader()
+
+# def Elements_pool():
+#   st.file_uploader(), st.file_uploader()
 
