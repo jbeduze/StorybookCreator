fbb60c5e847ff2a9c6f7650de69e0a1538a0c1af
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-10 23:50:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ st.write('Hello fools!')
 with st.form('Story_form'):
     with st.expander('',expander=True):
 
-    options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
-    radio_cols = st.columns([.25,10])
-    step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
-    if step == 'Initial story Elements':
-      st.markdown("Let's get started, please fill out the following fields:")
-      lmnt_cols = st.columns(2)
-      lmnt_cols[0].text_input('relation')
-      lmnt_cols[1].text_input('Name')
+        options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
+        radio_cols = st.columns([.25,10])
+        step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
+        if step == 'Initial story Elements':
+          st.markdown("Let's get started, please fill out the following fields:")
+          lmnt_cols = st.columns(2)
+          lmnt_cols[0].text_input('relation')
+          lmnt_cols[1].text_input('Name')
 
-      submitted = st.form_submit_button('Pull Elements')
+        submitted = st.form_submit_button('Pull Elements')
 
 
 
