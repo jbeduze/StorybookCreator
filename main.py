0c1aabd2253459067d95aaaf68292ff997f0d21a
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 00:33:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ with st.form('Story elements Form'):
                 st.markdown("Let's get started, please fill out the following fields:")
                 'upload a pciture of a loved one here:'
                 uploaded_img = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
-                if uploaded_img is not None:
-                        from PIL import Image
-                        image = Image.open(uploaded_img)
-                else:
-                        st.warning('Please upload at least one image of your loved one')
+                #if uploaded_img is not None:
+                        #from PIL import Image
+                        #image = Image.open(uploaded_img)
+                #else:
+                        #st.warning('Please upload at least one image of your loved one')
                 lmnt_cols = st.columns(2)
                 lmnt_cols[0].text_input('Name of loved one')
                 lmnt_cols[0]
