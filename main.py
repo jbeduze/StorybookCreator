cf8609d383a769aaed43c43f1995af74e9823279
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 00:25:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 st.write('Hello fools!')
 
 with st.form('Story elements Form'):
@@ -8,11 +7,24 @@ with st.form('Story elements Form'):
         step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
         if step == 'Initial story Elements':
                 st.markdown("Let's get started, please fill out the following fields:")
+                'upload a pciture of a loved one here:'
+                uploaded_img = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
+  
+          # Check if an image has been uploaded
+          if uploaded_img is not None:
+                  from PIL import Image
+                  image = Image.open(uploaded_img)
+          else:
+                  st.warning('Please upload at least one image of your loved one')
                 lmnt_cols = st.columns(2)
-                lmnt_cols[0].text_input('relation')
-                lmnt_cols[1].text_input('Name')
+                lmnt_cols[0].text_input('Name of loved one')
+                lmnt_cols[0]
+                lmnt_cols[1].text_input('Relation to loved one')
 
         submitted = st.form_submit_button('Pull Elements')
+
+        if step == 'AI Story Builder':
+                #ai chat will receive the elements inputs
 
 
 
