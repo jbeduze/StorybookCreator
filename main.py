9619b41d23b93404aa2d78e6ae3dfb70ce4d011f
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 01:25:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.write('Hello fools!')
 
-with st.form('Story elements Form'):
+with st.form('Story elements Form', layout='wide'):
         options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'Download Completed Story']
         radio_cols = st.columns([.25,10])
         step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
@@ -21,10 +21,34 @@ with st.form('Story elements Form'):
                         "Choose the Genre:",
                         options=["Adventure", "Fairy Tale", "Animal Story", "Space Exploration", "Magical Fantasy", "surprise me"]
                 )
-                
-                relation = lmnt_cols[1].text_input('Relation to loved one')
-
-        submitted = st.form_submit_button('Pull Elements')
+                        supporting_character = st.selectbox(
+                        "Choose A Supporting Character:",
+                        options=["Wise Owl", "Funny Clown", "Talking Robot", "Friendly Dragon", "Mischievous Monkey", "surprise me"],
+                )
+                        theme = st.selectbox(
+                        "Choose the Theme:",
+                        options=["Friendship", "Courage", "Kindness", "Discovery", "Teamwork", "surprise me"]
+                )
+                        tone = st.selectbox(
+                        "Choose the Tone/Mood:",
+                        options=["Joyful", "Exciting", "Mysterious", "Playful", "Heartwarming", "surprise me"]
+                )
+                with lmnt_col[1]:
+                        relation = st.text_input('Relation to loved one')
+                        setting = st.selectbox(
+                        "Choose the Setting:",
+                        options=["Enchanted Forest", "Outer Space", "Kingdom", "Under the Sea", "Mystical Mountain", "surprise me"]
+                )
+                        plot_elements = st.selectbox(
+                        "Choose Plot Elements:",
+                        options=["A Hidden Treasure", "A Grand Festival", "A Magic Spell", "A Daring Rescue", "A Secret Door", "surprise me"]
+                )
+                        magical_objects = st.selectbox(
+                        "Choose Magical Objects:",
+                        options=["Magic Wand", "Enchanted Mirror", "Golden Key", "Flying Carpet", "Invisible Cloak", "surprise me"]
+                )
+                        
+        submitted = st.form_submit_button('Pull Initial Elements')
 
         if step == 'AI Story Builder':
                 st.write('ai chat will receive the elements inputs')
