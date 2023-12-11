90532a8cefdfe4f3a094702ad2963eda4a379913
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 00:15:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ with st.form('Story elements Form'):
         options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
         radio_cols = st.columns([.25,10])
         step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
-            if step == 'Initial story Elements':
-              st.markdown("Let's get started, please fill out the following fields:")
-              lmnt_cols = st.columns(2)
-              lmnt_cols[0].text_input('relation')
-              lmnt_cols[1].text_input('Name')
+        if step == 'Initial story Elements':
+                st.markdown("Let's get started, please fill out the following fields:")
+                lmnt_cols = st.columns(2)
+                lmnt_cols[0].text_input('relation')
+                lmnt_cols[1].text_input('Name')
 
         submitted = st.form_submit_button('Pull Elements')
 
