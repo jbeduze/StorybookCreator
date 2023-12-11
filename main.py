08f64c3af36206f8dc1156ac56a78d67f1057f67
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 00:12:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import streamlit as st
 
 st.write('Hello fools!')
 
-
-exp = st.expander("story form")
-        exp = options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
-            radio_cols = st.columns([.25,10])
-            step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
+with st.form('Story elements Form'):
+        options = ['Initial story Elements','AI Story Builder', 'Build out Summary', 'download']
+        radio_cols = st.columns([.25,10])
+        step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
             if step == 'Initial story Elements':
               st.markdown("Let's get started, please fill out the following fields:")
               lmnt_cols = st.columns(2)
