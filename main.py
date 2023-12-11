3292d37efd6a77924b7fb9682bb8055de646093a
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 01:35:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('Storybook creator')
-st.write('Welcome to the story book creator, a place where AI technology can make any person on the planet the main character! Don't believe me? Give it a shot for yourself!')
+st.markdown('Welcome to the story book creator, a place where AI technology can make any person on the planet the main character! Don't believe me? Give it a shot for yourself!')
 
 with st.form('Story elements Form'):
         options = ['Initial story Elements','AI Story Builder', 'Build Summary', 'Download Story']
