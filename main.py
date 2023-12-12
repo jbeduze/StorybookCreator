9e4710a1219957b07e93c0cbee8c1bdb0ef3edcf
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 21:50:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 st.title('Storybook Creator')
 st.markdown("Welcome to the story book creator, a place where AI technology can make any person on the planet the main character! Don't believe me? Give it a shot for yourself!")
 
-with st.form('Story elements Form'):
-        options = ['Initial story Elements','AI Story Builder', 'Build Summary', 'Download Story']
-        radio_cols = st.columns([.25,10])
-        step = radio_cols[1].radio(label='', label_visibility='collapsed', options=options, horizontal=True, index=0)
-        if step == 'Initial story Elements':
+
+tab_INI, tab_AI, tab_BUI, tab_DWN = st.tabs[('Initial story Elements','AI Story Builder', 'Build Summary', 'Download Story')]
+
+with tab_INI:
+        with st.form('Story elements Form'):
                 st.markdown("Let's get started, please fill out the following fields:")
                 'upload a pciture of a loved one here:'
                 uploaded_img = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
@@ -53,6 +53,6 @@ with st.form('Story elements Form'):
         if submitted:
                 st.write(f"I'd like you to create a story about my {relation}, {name_of_loved}. The story's Genre will be: {genre}, with a setting as: {setting}. A supporting character will be: {supporting_character}, with a plot element of: {plot_elements}. The theme of the story is: {theme}, a magical object, included somewhere in the story is a: {magical_objects}, and the tone will be: {tone} ")
         if step == 'AI Story Builder':
-                st.write('ai chat will receive the elements inputs')
+        st.write('ai chat will receive the elements inputs')
 
 #'AI Story Builder', 'Build Summary', 'Download Story'
