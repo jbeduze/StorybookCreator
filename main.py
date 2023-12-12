244b03f933cbe4ceadb30752eeb1c1094b552d9a
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-12-11 22:18:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -49,9 +49,10 @@ with tab_INI:
                         options=["Magic Wand", "Enchanted Mirror", "Golden Key", "Flying Carpet", "Invisible Cloak", "surprise me"]
                 )
                         
-                submitted = st.form_submit_button('Pull Initial Elements')
-                if submitted:
-                        st.write(f"I'd like you to create a story about my {relation}, {name_of_loved}. The story's Genre will be: {genre}, with a setting as: {setting}. A supporting character will be: {supporting_character}, with a plot element of: {plot_elements}. The theme of the story is: {theme}, a magical object, included somewhere in the story is a: {magical_objects}, and the tone will be: {tone} ")
+                submitted_elements = st.form_submit_button('Pull Initial Elements')
+                if submitted_elements:
+                        st.write("We've collected all of your submitted inform,ation and submitted it to our custom Ai Chat, pop over to the next tab to see the AI at work!")
+                        with tab_AI: st.write(f"I'd like you to create a story about my {relation}, {name_of_loved}. The story's Genre will be: {genre}, with a setting as: {setting}. A supporting character will be: {supporting_character}, with a plot element of: {plot_elements}. The theme of the story is: {theme}, a magical object, included somewhere in the story is a: {magical_objects}, and the tone will be: {tone} ")
 with tab_AI:
         st.write('ai chat will receive the elements inputs')
 
